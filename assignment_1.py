a9aade7266984e1271be7dca8d15b32efcd1d3b2
yunhonam-main <namyun.global@gmail.com>
2024-12-27 20:54:37 +0700

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,71 @@
+import math
+import random
+
+if __name__ == "__main__":
+    '''
+    TASK 1
+    Print "My name is [Your Name]." 
+    Print "[1 sentence describing yourself]."
+    Use two prints but the strings must be printed in one line like this:
+    "My name is [Your Name]. [1 sentence describing yourself]."
+    '''
+    print()
+
+
+    '''
+    TASK 2
+    '''
+    question1 = input("How old are you? ")
+    # Do the calculation to find out the year of birth from input above
+    calculation = question1
+    # Then print "Ok, so you were born in [year]."
+    print()
+
+
+    '''
+    TASK 3    
+    '''
+    question2 = input("Say something and I will repeat what you say in all caps: ")
+    # Print answer in all capital letters
+    print()
+
+    '''
+    TASK 4
+    Ask question "Say something and I will count all the characters you used: "
+    Then print the number of characters used including blank spaces
+    '''
+    question3 = input("Say something and I will count all the characters you used: ")
+    print()
+
+    '''
+    TASK 5
+    Ask question "Say something and I will count only the letters you used: "
+    Then print only the number of letters used WITHOUT blank spaces
+    You will need to use a method called replace. (Reserach it!)
+    '''
+    question4 = input("Say something and I will count only the letters you used: ")
+    print()
+
+    '''
+    TASK 6
+    '''
+    num1 = 81
+    print("Square root of num1:")
+    # Print square root of num1
+    print()
+
+    '''
+    TASK 7
+    '''
+    radius = 7
+    print("Area of circle:")
+    # Print the area of circle with the given radius
+    print()
+
+    '''
+    TASK 8
+    Tip: Use a standard library called random and use its method randint
+    '''
+    print("Random number between 0 and 1000:")
+    # Print a randomized number between 0 and 1000
+    print()
